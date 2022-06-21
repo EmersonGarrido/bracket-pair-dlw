bf1f561ef55e98da3450a3395f343625ed755c91
EmersonGarrido <emersongarrido@hotmail.com.br>
2022-06-21 17:10:47 -0400

    chore: update

--- src/extension.ts
+++ src/extension.ts
@@ -2,15 +2,15 @@ import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
-    "bracket-pair-toggler.run",
+    "bracket-pair-color-dlw.run",
     () => {
       const workspaceConfiguration = vscode.workspace.getConfiguration(
-        "editor.bracketPairColorization"
+        "editor.bracketPairColorDLW"
       );
       const isEnabled = workspaceConfiguration.get<boolean>("enabled");
 
       if (isEnabled === undefined) {
-        const message = `Bracket Pair Colorization setting was not found`;
+        const message = `Bracket Pair Color setting was not found`;
 
         return vscode.window.showErrorMessage(message);
       }
@@ -19,14 +19,14 @@ export function activate(context: vscode.ExtensionContext) {
         .update("enabled", !isEnabled, vscode.ConfigurationTarget.Global)
         .then(
           () => {
-            const message = `Bracket Pair Colorization is ${
+            const message = `Bracket Pair Color is ${
               !isEnabled ? "enabled" : "disabled"
             }`;
 
             vscode.window.showInformationMessage(message);
           },
           () => {
-            const message = `Error toggling Bracket Pair Colorization`;
+            const message = `Error toggling Bracket Pair color`;
 
             vscode.window.showErrorMessage(message);
           }
